f30f49978a734ddf32a14db18e5be3bd28034728
Sam Lin <samklin33@gmail.com>
2025-06-10 02:29:54 +0800

    finish dearch page

--- src/components/ui/Item_Detail.jsx
+++ src/components/ui/Item_Detail.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 export default function Item_Detail({
     description="",
@@ -6,7 +7,13 @@ export default function Item_Detail({
     Booked = false,
     cancelDisplay=()=>{}
 }) {
+    const { isloggedIn } = useAuth();
     const [booked,setBooked] = useState(Booked)
+
+    const handleBook = async () => {
+
+    }
+    
     return(
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 animate-in fade-in duration-300">
         <div className="layout-container flex flex-col max-w-[960px] w-full bg-[#111811] rounded-xl shadow-lg px-10 py-8">
@@ -47,14 +54,16 @@ export default function Item_Detail({
             Booking Information
             </h2>
             <div className="p-4 grid grid-cols-[20%_1fr] gap-x-6">
-            <div className="col-span-2 grid grid-cols-subgrid border-t border-t-[#3b543b] py-5">
-                <p className="text-[#9cba9c] text-sm font-normal leading-normal">
-                Booked yet ?
-                </p>
-                <p className="text-white text-sm font-normal leading-normal">
-                {booked?"Yes":"None"}
-                </p>
-            </div>
+            {isloggedIn && 
+                <div className="col-span-2 grid grid-cols-subgrid border-t border-t-[#3b543b] py-5">
+                    <p className="text-[#9cba9c] text-sm font-normal leading-normal">
+                        Booked yet ?
+                    </p>
+                    <p className="text-white text-sm font-normal leading-normal">
+                        {booked?"Yes":"None"}
+                    </p>
+                </div>
+            }
             </div>
             <div className="flex flex-row px-4 py-3 gap-4 justify-evenly">
             <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-[#283928] text-white text-sm font-bold leading-normal tracking-[0.015em] hover:bg-[#c5d4c5] hover:text-[#283928] transition-all" 
@@ -62,11 +71,13 @@ export default function Item_Detail({
             >
                 <span className="truncate">Back</span>
             </button>
-            <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-[#283928] text-white text-sm font-bold leading-normal tracking-[0.015em] hover:bg-[#c5d4c5] hover:text-[#283928] transition-all" 
-                onClick={()=>setBooked(true)}
-            >
-                <span className="truncate">Booked ?</span>
-            </button>
+            {isloggedIn && 
+                <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-[#283928] text-white text-sm font-bold leading-normal tracking-[0.015em] hover:bg-[#c5d4c5] hover:text-[#283928] transition-all" 
+                    onClick={()=>setBooked(true)}
+                >
+                    <span className="truncate">Booked ?</span>
+                </button>
+            }
             </div>
 
         </div>
